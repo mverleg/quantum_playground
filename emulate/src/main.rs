082eb76bf948725b692799a4c5569e1d6eaf00f0
Mark <mverleg.noreply@gmail.com>
2018-10-12 19:26:14 +0200

    Wavefunction collapse working for observation of all qubits

--- emulate/src/main.rs
+++ emulate/src/main.rs
@@ -2,12 +2,14 @@
 //TODO @mark: allow dead code during dev
 #![allow(dead_code)]
 
+extern crate core;
 extern crate ndarray;
 extern crate num_complex;
-extern crate core;
+extern crate rand;
 
-use num_complex::Complex;
 use core::fmt::{Display, Error, Formatter};
+use num_complex::Complex;
+use rand::Rng;
 
 //TODO @mark: turn State into a type (which wraps a usize and is printable as |010>)
 
@@ -19,6 +21,19 @@ pub fn zero() -> Complex<f64> {
 pub fn one() -> Complex<f64> {
     // Cannot import Complex::one() for some reason
     return Complex::new(1., 0.)
+}
+
+pub fn weighted_choice<R: Rng>(weights: &Vec<f64>, mut rng: R) -> usize {
+    assert!((weights.iter().sum::<f64>() - 1.) < 1e-8);
+    let mut cumsum = 0.;
+    let choice = rng.gen();
+    for (k, w) in weights.iter().enumerate() {
+        cumsum += w;
+        if cumsum >= choice {
+            return k
+        }
+    }
+    unreachable!();
 }
 
 pub trait QuantumState {
@@ -39,28 +54,32 @@ struct Qubit(Complex<f64>, Complex<f64>);
 /// Entangled (sub)system
 /// Combination of 'entangled' and 'ensemble', haha!
 // Storage order: |0..00>, |0..01>, |0..10>, ..., |1..11>
-struct Entangble {
+struct Entangble<R: Rng> {
     qubits: usize,
     states: usize,
-    wf: Vec<Complex<f64>>
+    wf: Vec<Complex<f64>>,
+    rng: R,
 }
 
-impl Entangble {
-    pub fn new(qubits: usize) -> Self {
+impl <R: Rng> Entangble<R> {
+    pub fn new(qubits: usize, rng: R) -> Self {
         assert!(qubits > 0);
         if qubits > 5 {
             eprintln!("Emulating a quantum computer with {} qubits may not finish in feasible amount of time", qubits)
         }
         let states = 2usize.pow(qubits as u32);
-        let mut ent = Entangble { qubits, states, wf };
+        let wf = vec![zero(); states];
+        let mut ent = Entangble { qubits, states, wf, rng };
         ent.set_pure(0);
         ent
     }
 
     /// Collapse into one pure state
     fn set_pure(&mut self, index: usize) {
-        let mut wf = vec![zero(); states];
-        wf[0] = one();
+        for wfi in self.wf.iter_mut() {
+            *wfi = zero();
+        }
+        self.wf[index] = one();
     }
 
     /// Calculate the classical probabilities (which one wouldn't be able to do on a real quantum computer, but can be done on the emulator).
@@ -70,11 +89,11 @@ impl Entangble {
 
     /// Check that the total occupation is still unity
     pub fn check_norm(&self) {
-        assert!((self.wf.iter().map(|v| v.norm()).sum::<f64>() - 1.) < 1e-8);
+        assert!((self.calc_probs().iter().sum::<f64>() - 1.) < 1e-8);
     }
 }
 
-impl QuantumState for Entangble {
+impl<R: Rng> QuantumState for Entangble<R> {
     fn observe(&mut self) -> usize {
         let probs = self.calc_probs();
         let pick = weighted_choice(&probs, &mut self.rng);
@@ -84,7 +103,7 @@ impl QuantumState for Entangble {
     }
 }
 
-impl Display for Entangble {
+impl<R: Rng> Display for Entangble<R> {
     fn fmt<'a>(&self, f: &mut Formatter<'a>) -> Result<(), Error> {
         println!("{}-state entangled quantum system:", self.qubits);
         for j in 0 .. self.states {
